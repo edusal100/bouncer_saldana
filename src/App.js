b9ef2605a45be55d88c8b79ecf308d4c59f7a66a
eduardo saldana <edusal100@hotmail.com>
2022-06-11 21:38:17 -0500

    Chakra UI and NavBar added/stylized

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-import NavBar from './Components/NavBar/NavBar';
+import WithSubnavigation from './Components/NavBar/WithSubnavigation';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <NavBar />
+        <WithSubnavigation />
       </header>
     </div>
   );
