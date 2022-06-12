07524e9bf849176e6dfd4618a44cd0220a8f1c08
eduardo saldana <edusal100@hotmail.com>
2022-06-11 20:33:09 -0500

    Navbar Component - Created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './Components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Bouncer Store by Eduardo Saldana
-        </p>
+        <NavBar />
       </header>
     </div>
   );
